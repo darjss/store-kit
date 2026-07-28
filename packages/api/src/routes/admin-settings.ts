b6828b13cc4d1bcd122bc45e7ad27cd8d4ead74f
darjss <darjsavid@gmail.com>
2026-07-28 16:27:45 +0800

    feat(api): add guarded admin settings routes

---
+++ packages/api/src/routes/admin-settings.ts
@@ -0,0 +1,15 @@
+import { commerce } from '@store-kit/commerce'
+import { adminStoreSettingsUpdateSchema } from '@store-kit/contracts/admin-settings'
+import { Result } from 'better-result'
+
+import { contractBody } from '~/typebox-contract'
+
+import { createApprovedAdminRoutes } from './approved-admin'
+
+export const adminSettingsRoutes = createApprovedAdminRoutes()
+  .get('/settings/store', async () => Result.serialize(await commerce.settings.getStore()))
+  .put(
+    '/settings/store',
+    async ({ body }) => Result.serialize(await commerce.settings.updateStore(body)),
+    { body: contractBody(adminStoreSettingsUpdateSchema) },
+  )
